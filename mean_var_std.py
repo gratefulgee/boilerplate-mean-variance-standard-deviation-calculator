b93cda7198e052e5d6bb2a7579c91f80a577671f
gratefulgee <fitrahuljjihan@gmail.com>
2025-05-29 17:11:45 +0700

    updated mean_var_std py

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,18 +1,18 @@
 import numpy as np
 
-def calculate(list):
-    if len(list) != 9:
+def calculate(input_list):
+    if len(input_list) != 9:
         raise ValueError("list must contain nine numbers")
 
-    matrix = np.array(list).reshape(3,3)
+    matrix = np.array(input_list).reshape(3,3)
 
     calculations = {
-        'mean' : [matrix.mean(axis=0).tolist(), matrix.mean(axis=1).tolist(), matrix.mean().item()],
-        'variance': [matrix.var(axis=0).tolist(), matrix.var(axis=1).tolist(), matrix.var().item()],
-        'standard deviation': [matrix.std(axis=0).tolist(), matrix.std(axis=1).tolist(), matrix.std().item()],
-        'max': [matrix.max(axis=0).tolist(), matrix.max(axis=1).tolist(), matrix.max().item()],
-        'min': [matrix.min(axis=0).tolist(), matrix.min(axis=1).tolist(), matrix.min().item()],
-        'sum': [matrix.sum(axis=0).tolist(), matrix.sum(axis=1).tolist(), matrix.sum().item()]
+        'mean' : [list(np.mean(matrix, axis=0)), list(np.mean(matrix, axis=1)), np.mean(matrix)],
+        'variance': [list(np.var(matrix, axis=0)), list(np.var(matrix, axis=1)), np.var(matrix)],
+        'standard deviation': [list(np.std(matrix, axis=0)), list(np.std(matrix, axis=1)), np.std(matrix)],
+        'max': [list(np.max(matrix, axis=0)), list(np.max(matrix, axis=1)), np.max(matrix)],
+        'min': [list(np.min(matrix, axis=0)), list(np.min(matrix, axis=1)), np.min(matrix)],
+        'sum': [list(np.sum(matrix, axis=0)), list(np.sum(matrix, axis=1)), np.sum(matrix)]
     }
 
     return calculations
